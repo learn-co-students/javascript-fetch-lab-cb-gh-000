398add679cf586dae128f83c5bae3b9f42172d44
Qin LIN <linq.sophie@gmail.com>
2017-02-23 15:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
+const token = getToken();
+
 function getIssues() {
+  fetch('https://api.github.com/repos/Underwaternya/javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  var source = document.getElementById("issues-template").innerHTML;
+  var template = Handlebars.compile(source);
+  var issues = template(json);
+  document.getElementById("issues").innerHTML += issues;
 }
 
 function createIssue() {
+  const postData = {
+    title: document.getElementById("title").value,
+    body: document.getElementById("body").value
+  };
+
+  fetch('https://api.github.com/repos/Underwaternya/javascript-fetch-lab/issues', {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(getIssues());
 }
 
 function showResults(json) {
+  var source = document.getElementById("repo-template").innerHTML;
+  var template = Handlebars.compile(source);
+  var result = template(json);
+  document.getElementById("results").innerHTML += result;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab';
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/'+ repo +'/forks', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json));
 }
 
 function getToken() {
